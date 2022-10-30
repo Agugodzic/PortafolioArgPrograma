b3360aa41a33d36a0987fd6642d4ead494a5ba5b
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2022-10-30 17:47:28 -0300

    Update cursos.service.ts

--- Portafolio/src/app/servicios/cursos.service.ts
+++ Portafolio/src/app/servicios/cursos.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CursosService {
-  private apiServerUrl = "https://portafolio-back-ap.herokuapp.com";
+  private apiServerUrl = "http://localhost:8080";
   constructor(private http: HttpClient) { }
 
  public listar():Observable<CursoModel[]>{
